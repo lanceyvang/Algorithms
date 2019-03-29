f105dcfb1981b972f5fbb77578e90e5f3ef951dc
Yang Vang <lanceyvang@gmail.com>
2019-03-28 21:58:00 -0400

    finished levelWidth

---
+++ Algorithms/Level Width.js
@@ -0,0 +1,51 @@
+// --- Directions
+// Given the root node of a tree, return
+// an array where each element is the width
+// of the tree at each level.
+// --- Example
+// Given:
+//     0
+//   / |  \
+// 1   2   3
+// |       |
+// 4       5
+// Answer: [1, 3, 2]
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+  add(data) {
+    this.children.push(new Node(data));
+  }
+}
+
+const root = new Node(0);
+root.add(1);
+root.add(2);
+root.add(3);
+root.children[0].add(4);
+root.children[2].add(5);
+
+// expect(levelWidth(root)).toEqual([1, 3, 2]);
+
+const levelWidth = root => {
+  let arr = [root, 's'];
+  let counters = [0];
+
+  while (arr.length > 1) {
+    const node = arr.shift();
+
+    if (node === 's') {
+      counters.push(0);
+      arr.push('s');
+    } else {
+      arr.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
+  return counters;
+};
+
+console.log(levelWidth(root));
