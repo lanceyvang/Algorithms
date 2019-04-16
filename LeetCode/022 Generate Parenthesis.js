856613d2ec57de08c5860b22d264ed3e77562f52
Yang Vang <lanceyvang@gmail.com>
2019-04-16 18:51:17 -0400

    fin 022

---
+++ LeetCode/022 Generate Parenthesis.js
@@ -0,0 +1,27 @@
+/*
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+*/
+
+const generateParenthesis = n => {
+  let arr = [];
+  generate(n, n, '');
+  return arr;
+
+  function generate(left, right, str) {
+    if (!left && !right && str.length) arr.push(str);
+    if (left) generate(left - 1, right, str + '(');
+    if (right > left) generate(left, right - 1, str + ')');
+  }
+};
+
+console.log(generateParenthesis(3));
