b4deae825361989b929df55c5baccdc3c9346426
Yang Vang <lanceyvang@gmail.com>
2019-04-22 20:04:44 -0400

    refactored 031

--- LeetCode/031 Next Permutation.js
+++ LeetCode/031 Next Permutation.js
@@ -12,12 +12,48 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
 1,1,5 → 1,5,1
 */
 
-const nextPermutation = nums => {
-  if (nums.length <= 1) {
-    reverse(0);
-    return;
-  }
+// const nextPermutation = nums => {
 
+//   let i = findFirstDecreasingElement();
+
+//   if (i < 0) {
+//     reverse(0);
+//     return nums;
+//   } else {
+//     let j = findFirstIncreasingElement();
+//     swap(i, j);
+//     reverse(i + 1);
+//   }
+//   return nums;
+
+//   // helper functions
+//   function swap(a, b) {
+//     [nums[a], nums[b]] = [nums[b], nums[a]];
+//   }
+
+//   function reverse(start) {
+//     let end = nums.length - 1;
+//     while (start < end) {
+//       swap(start, end);
+//       start++;
+//       end--;
+//     }
+//   }
+
+//   function findFirstDecreasingElement() {
+//     let i = nums.length - 2;
+//     while (i >= 0 && nums[i] >= nums[i + 1]) i--;
+//     return i;
+//   }
+
+//   function findFirstIncreasingElement() {
+//     let j = nums.length - 1;
+//     while (j >= 0 && nums[j] <= nums[i]) j--;
+//     return j;
+//   }
+// };
+
+const nextPermutation = nums => {
   let i = nums.length - 2;
   while (i >= 0 && nums[i + 1] <= nums[i]) i--;
   if (i < 0) {
