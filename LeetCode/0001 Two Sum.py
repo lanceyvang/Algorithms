1a7ce2c7a3045d9d480331bce8951df18b6ccf2a
Lance <lanceyvang@gmail.com>
2019-09-29 22:02:31 -0400

    fin two_sum.py

---
+++ LeetCode/0001 Two Sum.py
@@ -0,0 +1,26 @@
+'''
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+'''
+
+def two_sum(nums, target):
+
+    d: dict = {}
+    
+    for index, num in enumerate(nums):
+        find_num: int = target - num
+        if find_num in d: 
+            return [d[find_num], index]
+        else:
+            d[num]: int = index
+
+    return []
+
+print(two_sum([5,75,25], 100))
